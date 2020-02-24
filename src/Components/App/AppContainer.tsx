fcd725b0c97d147bc25c14a9b2a8f8327136f0c3
peter_alfie <alfie1104@gmail.com>
2020-02-25 08:43:27 +0900

    Initial setup for CSS is done

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import reset from "styled-reset";
 import { IS_LOGGED_IN } from "./AppQueries";
+import AppPresenter from "./AppPresenter";
+import { createGlobalStyle, ThemeProvider } from "src/typed-component";
+import theme from "../../theme";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+`;
 
 const AppContainer = ({ data }: { data?: any }) => (
-  <div>{JSON.stringify(data)}</div>
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle />
+      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </>
+  </ThemeProvider>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
