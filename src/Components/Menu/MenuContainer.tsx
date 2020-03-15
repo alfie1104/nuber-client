9676441fe9a85c0b3468ca271434f5e105c72314
peter_alfie <alfie1104@gmail.com>
2020-03-16 08:36:08 +0900

    HomeSidebar is done

--- src/Components/Menu/MenuContainer.tsx
+++ src/Components/Menu/MenuContainer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import MenuPresenter from "./MenuPresenter";
+import { Query } from "react-apollo";
+import { userProfile } from "src/types/api";
+import { USER_PROFILE } from "src/sharedQueries";
 
+class ProfileQuery extends Query<userProfile> {}
 class MenuContainer extends React.Component {
   render() {
-    return <MenuPresenter />;
+    return (
+      <ProfileQuery query={USER_PROFILE}>
+        {({ data, loading }) => <MenuPresenter data={data} loading={loading} />}
+      </ProfileQuery>
+    );
   }
 }
 
