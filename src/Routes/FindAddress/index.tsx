5534a077fe616e10880dd031946a130dac6159ae
peter_alfie <alfie1104@gmail.com>
2020-03-30 13:51:51 +0900

    FindAddresss is being made

--- src/Routes/FindAddress/index.tsx
+++ src/Routes/FindAddress/index.tsx
@@ -1,2 +1,5 @@
+import { GoogleApiWrapper } from "google-maps-react";
 import FindAddressContainer from "./FindAddressContainer";
-export default FindAddressContainer;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCviX1EdIAkrq5hMKkf45F3mQBezIusnz0"
+})(FindAddressContainer);
