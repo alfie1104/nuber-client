881826646d34ebb62a09ac553fe7c46e1ea55ba5
peter_alfie <alfie1104@gmail.com>
2020-04-29 08:30:51 +0900

    Updates

---
+++ precache-manifest.67173bab317fb2367703682409f372e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86782f611a8ab9a62eb7c65b89e066ad",
+    "url": "/nuber-client/index.html"
+  },
+  {
+    "revision": "406536a372347549ca8b",
+    "url": "/nuber-client/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "406536a372347549ca8b",
+    "url": "/nuber-client/static/js/2.60d02cc8.chunk.js"
+  },
+  {
+    "revision": "428c7b193724e715944e57e016f42a30",
+    "url": "/nuber-client/static/js/2.60d02cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c905f5ebc15ac4f24bc",
+    "url": "/nuber-client/static/js/main.992a6e9f.chunk.js"
+  },
+  {
+    "revision": "097df25f0071e0b0c26d",
+    "url": "/nuber-client/static/js/runtime-main.0c494bf2.js"
+  },
+  {
+    "revision": "f1662d11088621fd43b5f95d795d749e",
+    "url": "/nuber-client/static/media/bg.f1662d11.png"
+  }
+]);
